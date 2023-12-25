dac3728e2503f6ddb88c4fad0184b1710c2388d7
Md. Robeul Islam <robeul.starit@gmail.com>
2023-12-25 20:00:29 +0600

    map

--- index.js
+++ index.js
@@ -623,3 +623,20 @@ let uniqueArray = [...mySet];
 console.log(uniqueArray);
 // Output: [1,4,3]
 /* Set Object */
+
+/* map */
+const personDetails = new Map();
+personDetails.set("name", "Alice");
+personDetails.set("age",30);
+
+console.log(personDetails.get("name"));
+// Output: "Alice"
+
+console.log(personDetails.has("age"));
+// Output: true
+
+personDetails.delete("age");
+console.log(personDetails.size)
+// Output: 1
+
+/* map */
